a72f44781ba3965d362d1b84bbd2084274dd497b
kimgoid8108 <kimgoid8108@gmail.com>
2025-11-07 10:06:51 +0900

    20iron

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
+import { Suspense } from "react";
 import BgImage from "./BgImage/BgImage";
 
-
-
 export default function Home() {
-  return <div>
-    <BgImage/>
-  </div>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <BgImage />
+    </Suspense>
+  );
 }
